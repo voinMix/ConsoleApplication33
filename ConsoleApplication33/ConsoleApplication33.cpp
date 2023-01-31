9fdc8e68e5d03b9c5c3052c23b9ac3f780de9473
student <student@BLUE16>
2023-01-31 18:31:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication33/ConsoleApplication33.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+#include <numeric> // iota можно использовать только при импортировании бибилиотеки numeric
+#include <algorithm>
+using namespace std;
+int main()
+{
+	array<int, 10> arr; // создание контейнера array
+
+	iota(arr.begin(), arr.end(), 1); // заполнение контейнера методом iota
+
+	for_each(arr.begin(), arr.end(), [](auto item) {cout << item << " "; }); // вывод контейнера через метод for_each для проверки
+	cout << endl;
+
+	vector<int> vec(10); // создание контейнера vector
+
+	reverse_copy(arr.begin(), arr.end(), vec.begin()); // заполнение контейнира методом обратного копирования из контейнера array
+
+	for_each(vec.begin(), vec.end(), [](auto item) {cout << item << " "; }); // вывод контейнера через метод for_each для проверки
+	cout << endl;
+
+	return 0;
+}
